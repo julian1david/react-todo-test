44aafdebdfa5c1f60fd73dccf5f1b0ec2fbb0aef
JulianF <juliandavidbf@gmail.com>
2022-07-13 18:45:13 -0500

    @feat: clean storage event

--- src/components/ChangeAlert/useStorageListener.jsx
+++ src/components/ChangeAlert/useStorageListener.jsx
@@ -7,12 +7,18 @@ export const useStorageListener = syncronized => {
 	const [storageChange, setStorageChange] = useState(false);
 
 	useEffect(() => {
-		window.addEventListener('storage', change => {
+		const onChange = change => {
 			if (change.key === 'TODO_V1') {
-				console.log('Hubo Cambios en Todos_V1');
+				console.log('Hubo cambios en TODOS_V1');
 				setStorageChange(true);
 			}
-		});
+		};
+
+		window.addEventListener('storage', onChange);
+
+		return () => {
+			window.removeEventListener('storage', onChange);
+		};
 	}, [storageChange]);
 
 	const toggleShow = () => {
