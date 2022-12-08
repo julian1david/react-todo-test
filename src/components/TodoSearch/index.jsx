a60e934c47e4e0d7e5ecbd8ac90615d704dd9eb6
Julian F <juliandavidbf@gmail.com>
2022-12-08 09:37:44 -0500

    format

--- src/components/TodoSearch/index.jsx
+++ src/components/TodoSearch/index.jsx
@@ -1,13 +1,11 @@
 import style from './TodoSearch.module.scss';
 
 function TodoSearch({ search, setSearch, loading }) {
-
 	const handleSearch = e => {
 		setSearch(e.target.value);
 	};
 
 	return (
-		
 		<input
 			className={style.SearchTodo}
 			type='text'
